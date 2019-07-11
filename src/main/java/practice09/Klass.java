5d25e1111cb7b2849af713b611d98e3d88dc1fd3
AhGang <474494594@qq.com>
2019-07-11 21:54:45 +0800

    完成TEST 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
 public class Klass {
+    private int num;
+    private Student student;
+    private String name;
+
+
+    private boolean isInClass = false;
+    public Klass(int num) {
+        this.num= num;
+    }
+
+    public int getNumber() {
+        return this.num;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.num;
+    }
+
+    public void assignLeader(Student jerry) {
+        System.out.print("It is not one of us.\n");
+        if(isInClass){
+            this.student = jerry;
+        }
+
+    }
+    public boolean isInClass() {
+        return isInClass;
+    }
+    public Student getLeader() {
+        return this.student;
+    }
+
+    public void appendMember(Student jerry) {
+        isInClass = true;
+        this.name = jerry.getName();
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,49 @@
 package practice09;
 
 public class Teacher {
+    private String name;
+    private int age;
+    private Klass klass;
+    private int id;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+    public Teacher(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+    public String introduce() {
+        if(klass==null){
+            return "My name is "+ this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I teach No Class.";
+        }else{
+            return "My name is "+ this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I teach Class " + klass.getNumber() + ".";
+        }
+    }
+
+
+    public String introduceWith(Student jerry) {
+        if(this.klass.getNumber() == jerry.getKlass().getNumber()){
+            return "My name is " + this.name + ". I am 21 years old. I am a Teacher. I teach "+jerry.getName()+".";
+        }else{
+            return "My name is " + this.name + ". I am 21 years old. I am a Teacher. I don't teach "+jerry.getName()+".";
+        }
+
+    }
 }
