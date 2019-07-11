0e963585ed2190072d076365f70a2948a292576c
AhGang <474494594@qq.com>
2019-07-11 20:16:54 +0800

    完成第六个测试关于Person、Student。Worker的属性获取和方法

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,43 @@
 package practice06;
 
 public class Teacher {
+    private String name;
+    private int age;
+    private int klass;
+
+    public Teacher(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public Teacher(String name, int age,int klass) {
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+    public String introduce() {
+        if(this.klass != 0){
+            return "My name is "+ this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I teach Class " + this.getKlass() + ".";
+        }else{
+            return "My name is "+ this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I teach No Class.";
+        }
+//        if(this.klass != 1){
+//            return "My name is "+ this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I teach " + this.getKlass()+" Class.";
+//        }else{
+//            return "My name is "+ this.getName() +". I am " + this.getAge() + " years old. I am a Teacher. I teach No Class.";
+//        }
+
+    }
+
+
 }
