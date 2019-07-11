2d38145605ccbc0e1f5590ead5297cba0b3c7617
AhGang <474494594@qq.com>
2019-07-11 20:01:28 +0800

    完成第四个测试关于Person、Student。Worker的属性获取和方法

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,23 @@
 package practice04;
 
 public class Worker {
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "My name is "+ this.getName() +". I am " + this.getAge() + " years old. I am a Worker. I have a job.";
+    }
 }
